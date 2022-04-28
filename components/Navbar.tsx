31fed8c8503c298e70f0dbd24684728617dee379
LoQeN00 <LoQeN00@o2.pl>
2022-04-28 17:58:44 +0200

    added backing to dashboard

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useSession, signOut } from 'next-auth/react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 type Props = {}
 
@@ -9,18 +10,22 @@ const Navbar = (props: Props) => {
 
   return (
     <div className="flex items-center justify-between border-b border-[#393a3b] p-5">
-      <div className="flex items-center space-x-2">
-        <p className="text-xl font-bold text-[#e4e6eb]">{session?.user.name}</p>
-        <div className="relative h-12 w-12">
-          {session?.user.image && (
-            <Image
-              src={session?.user.image}
-              layout="fill"
-              className="rounded-full"
-            />
-          )}
+      <Link href="/" passHref>
+        <div className="flex items-center space-x-2">
+          <p className="text-xl font-bold text-[#e4e6eb]">
+            {session?.user.name}
+          </p>
+          <div className="relative h-12 w-12">
+            {session?.user.image && (
+              <Image
+                src={session?.user.image}
+                layout="fill"
+                className="rounded-full"
+              />
+            )}
+          </div>
         </div>
-      </div>
+      </Link>
       <div>
         <button
           className="ease rounded-md border border-[#e4e6eb] py-2 px-4 text-lg font-bold text-[#e4e6eb] transition-all hover:bg-[#e4e6eb] hover:text-[#393a3b]"
